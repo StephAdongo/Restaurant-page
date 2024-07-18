137aa6d882943c0ce55981fac16ad488cb2e9f91
StephAdongo <stephanie@kabarak.ac.ke>
2024-07-18 18:45:06 +0300

    add webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,29 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
+
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+    },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin ({
         template:'./src/index.html',
