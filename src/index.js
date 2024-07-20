5c5d42f7af99e86d3978ceda292e15acb995217a
StephAdongo <stephanie@kabarak.ac.ke>
2024-07-20 11:45:42 +0300

    add .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import './styles.css';
-import home from './home';
-import menu from './menu';
-import about from './about';
+import  home  from "./home";
+import menu from "./menu";
+import about from "./about";
 
 function index() {
-    const homeButton = document.querySelector('#home');
-    const aboutButton = document.querySelector('#about');
-    const menuButton = document.querySelector('#menu');
+const homeButton = document.querySelector('#home');
+const menuButton = document.querySelector('#menu');
+const aboutButton = document.querySelector('#about');
 
-    function homecontent () {
-        console.log('home');
-        menuButton.classList.remove('tab');
+function homecontent() {
+  console.log('home');
+  menuButton.classList.remove('tab');
   aboutButton.classList.remove('tab');
   homeButton.classList.add('tab');
   const content = document.querySelector('#content');
@@ -18,34 +18,33 @@ function index() {
   const homect = home();
   content.appendChild(homect);
 }
+
 function menucontent() {
-    console.log('menu');
-    homeButton.classList.remove('tab');
-    aboutButton.classList.remove('tab');
-    menuButton.classList.add('tab');
-    const content = document.querySelector('#content');
-    content.textContent = '';
-    const menuct = menu();
-    content.appendChild(menuct);
-  }
-  
-  function aboutcontent() {
-    console.log('about');
-    homeButton.classList.remove('tab');
-    menuButton.classList.remove('tab');
-    aboutButton.classList.add('tab');
-    const content = document.querySelector('#content');
-    content.textContent = ''
-    const aboutct = about();
-    content.appendChild(aboutct);
-  }
-  
-  homeButton.addEventListener("click", function() {homecontent()});
-  menuButton.addEventListener("click", function() {menucontent()});
-  aboutButton.addEventListener("click", function() {aboutcontent()});
-  
-  homecontent();
-  }
-  index();   
+  console.log('menu');
+  homeButton.classList.remove('tab');
+  aboutButton.classList.remove('tab');
+  menuButton.classList.add('tab');
+  const content = document.querySelector('#content');
+  content.textContent = '';
+  const menuct = menu();
+  content.appendChild(menuct);
+}
 
+function aboutcontent() {
+  console.log('about');
+  homeButton.classList.remove('tab');
+  menuButton.classList.remove('tab');
+  aboutButton.classList.add('tab');
+  const content = document.querySelector('#content');
+  content.textContent = ''
+  const aboutct = about();
+  content.appendChild(aboutct);
+}
 
+homeButton.addEventListener("click", function() {homecontent()});
+menuButton.addEventListener("click", function() {menucontent()});
+aboutButton.addEventListener("click", function() {aboutcontent()});
+
+homecontent();
+}
+index();
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,38 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    
   },
-
   module: {
-    rules:[
+    rules: [
       {
-      test: /\.css$/i,
+        test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-    },
+      },
     ],
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
-
   plugins: [
-    new HtmlWebpackPlugin ({
-        template:'./src/index.html',
-        filename: 'index.html',
-        inject: 'head',
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'head',
       scriptLoading: 'defer',
     })
   ],
-  
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
